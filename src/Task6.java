9c6a3aadcc0d4bf97085f2853d944694fe056832
skmeyer <sarahbutwritingeditioN@gmail.com>
2025-02-17 17:27:36 -0500

    Task 6 Complete

--- src/Task6.java
+++ src/Task6.java
@@ -2,12 +2,12 @@ public class Task6
 {
     public static void main(String[] args)
     {
-        for(int row = 1; row <= 5; row++)
+        for(int row = 5; row >= 1; row--)
         {
-            System.out.println("");
-            for(int col = 1; col <= 5; col++)
+            System.out.println();
+            for(int col = 1; col <= row; col++)
             {
-                System.out.println(row * col);
+                System.out.print(" *");
             }
         }
     }

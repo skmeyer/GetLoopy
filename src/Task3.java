52a2fa09c7adb2f7b23a9ae82d80c67ff6f127d7
skmeyer <sarahbutwritingeditioN@gmail.com>
2025-02-17 16:30:06 -0500

    Task 3 Complete

---
+++ src/Task3.java
@@ -0,0 +1,10 @@
+public class Task3
+{
+    public static void main(String[] args)
+    {
+        for(int n = 0; n < 19; n+=3)
+        {
+            System.out.print(" | " + n);
+        }
+    }
+}

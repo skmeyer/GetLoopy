83b1aa76bd80fd2cf0302e8b08132421b991bcfa
skmeyer <sarahbutwritingeditioN@gmail.com>
2025-02-17 16:32:08 -0500

    Task 4 Complete

---
+++ src/Task4.java
@@ -0,0 +1,10 @@
+public class Task4
+{
+    public static void main(String[] args)
+    {
+        for(int n = 10; n > -1; n-=2)
+        {
+            System.out.print(" | " + n);
+        }
+    }
+}
